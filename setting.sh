be092d7253118c2d61fe8af548680940ab592648
shyeonn <tkdgus2916@gmail.com>
2024-03-26 12:13:54 +0000

    Add github account setting

--- setting.sh
+++ setting.sh
@@ -4,11 +4,15 @@ NVIM_PATH=~/.config/nvim
 
 mkdir -p $NVIM_PATH
 
+git config --global user.email "tkdgus2916@gmail.com"
+git config --global user.name "shyeonn"
+
 
 ln -s ~/$CUR_DIR/.vimrc ~/.vimrc
 ln -s ~/$CUR_DIR/init.vim $NVIM_PATH/init.vim
 ln -s ~/$CUR_DIR/.tmux.conf ~/.tmux.conf
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
